ecec5908eb1dfa4e738412f9e7c3958e66512893
84danie <danielleholzberger@gmail.com>
2017-10-22 11:21:59 -0700

    Added boilerplate form for createflashcard

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TopNavbar from './components/TopNavbar';
 import App from './components/App';
+import CreateFlashcard from './components/CreateFlashcard';
 
 ReactDOM.render(
   <TopNavbar />, document.getElementById('top-navbar')
 );
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <CreateFlashcard />, document.getElementById('create-flashcard')
 );
 
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
