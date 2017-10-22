5697d538e05cfc61e4cf13051ec6886be208c249
84danie <danielleholzberger@gmail.com>
2017-10-22 12:31:05 -0700

    Added input for term and definition

--- src/components/CreateFlashcard.jsx
+++ src/components/CreateFlashcard.jsx
@@ -1,32 +1,47 @@
 import React from 'react'
-import { Form , Text} from 'react-input'
+import { Table } from 'reactstrap';
+import FlashcardList from './FlashcardList';
 
 class CreateFlashcard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {term: '',definition: ''};
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange1(event) {
+    this.setState({term: event.target.value});
+  }
+  handleChange2(event) {
+    this.setState({definition: event.target.value});
   }
   handleSubmit(event){
-    alert('A name was submitted: ' + this.state.value);
+    alert('New flashcard created: ' + this.state.term +' '+ this.state.definition);
     event.preventDefault();
   }
   render(){
     return (
+      <div>
+      <Table bordered>
+      <tbody>
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-            <input type="text" value = {this.state.value}
-            onChange={this.handleChange} />
+          Term:
+            <input type="text" term = {this.state.term}
+            onChange={this.handleChange1} />
+          Definition:
+            <input type="text" definition = {this.state.definition}
+            onChange={this.handleChange2} />
         </label>
         <input type="submit" value="Submit" />
         </form>
+        </tbody>
+        </Table>
+        </div>
+
     );
   }
 }
