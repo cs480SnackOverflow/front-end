dd02572e285741ae3b61cd5a315be9813c1ad89a
Bao Chau <chauquocbao0907@gmail.com>
2017-11-19 22:30:48 -0800

    add introduction voice

--- src/components/LoadFlashcardSet.jsx
+++ src/components/LoadFlashcardSet.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import FlashcardList from './FlashcardList'
-import axios from 'axios'
+import React from 'react';
+import FlashcardList from './FlashcardList';
+import axios from 'axios';
+import annyang from 'annyang';
+
 class LoadFlashcardSet extends React.Component {
 
   constructor(props) {
@@ -11,10 +13,18 @@ class LoadFlashcardSet extends React.Component {
     };
   }
 
+  sayIntroduction() {
+    let str = 'Welcome to Commuter Study. Would you like to study or test?';
+    return str.split().join('+');
+  }
+
   getInitialState() {
     return {flashcards: [],title:''};
   }
 
+
+
+  
   componentDidMount() {
     axios.get('/api/flashcards').then(response => {
       this.setState({flashcards: response.data._embedded.flashcards});
@@ -22,7 +32,17 @@ class LoadFlashcardSet extends React.Component {
     });
   }
   render() {
-    return (<FlashcardList flashcards={this.state.flashcards} title={this.state.title}/>);
+    let audioSrc = 'http://commuterstudy.com/audio?msg=' + this.sayIntroduction();
+    console.log(audioSrc);
+    return (
+      <div>
+        <FlashcardList flashcards={this.state.flashcards} title={this.state.title}/>
+        <audio autoPlay>
+          <source src={ audioSrc } type='audio/mpeg'/>
+            Your browser does not support the audio element.
+        </audio>
+      </div>
+    );
   }
 }
 
