0ee2e0fc4f0cf07e39c9a6b6d6cea9ee957dc127
84danie <danielleholzberger@gmail.com>
2017-11-24 17:33:43 -0800

    Implemented links to buttons on landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
-import { Container, Row, Col } from 'reactstrap';
-import Banner from 'react-banner'
 import 'react-banner/dist/style.css'
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Route } from 'react-router-dom';
 
-
+const CreateButton = ({ title, history }) => (
+  <Button
+    color="primary" size="lg"
+    onClick={() => history.push('/create')}
+  >
+    {title}
+  </Button>
+);
+const NavCreate = () => (
+  <Route path="/" render={(props) => <CreateButton {...props}title="Create" />} />
+)
+const StudyButton = ({ title, history }) => (
+  <Button
+    color="primary" size="lg"
+    onClick={() => history.push('/sets')}
+  >
+    {title}
+  </Button>
+);
+const NavStudy = () => (
+  <Route path="/" render={(props) => <StudyButton {...props}title="Study" />} />
+)
 class LandingPage extends Component {
   render() {
     return (
       <div>
       <div className="content">
         <h1>Commuter Study</h1>
+        <p>Learn more on the go.</p>
         <ul className="header">
-          <li> <Button color="primary" size="lg">  Create</Button></li>
-          <li> <Button color="primary" size="lg">Study  </Button>{' '}{' '}</li>
+          <li> <NavCreate/></li>
+          <li> <NavStudy/></li>
           <li> <Button outline color="primary" size="lg">Learn More      </Button>{' '}</li>
         </ul>
       </div>
