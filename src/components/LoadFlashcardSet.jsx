25dc6d9f8b040a9e609bef659f6cbc3d210e65d0
Bao Chau <chauquocbao0907@gmail.com>
2017-11-25 16:41:27 -0800

    Use anyang for voice control (#26)
    * add more voice recognition part
    
    * add testing mode for set page
    
    * fix asynchronous problem with test mode

--- src/components/LoadFlashcardSet.jsx
+++ src/components/LoadFlashcardSet.jsx
@@ -7,42 +7,130 @@ class LoadFlashcardSet extends React.Component {
 
   constructor(props) {
     super(props);
+    
     this.state = {
-      flashcards: [],
-      title: ''
+      flashcards: [
+      ],
+      title: '',
     };
+    this.flashcardStatus = [];
+    this.correctAnswers = 0;
   }
 
   getFlashcards() {
-    axios.get('/api/flashcards').then(response => {
+    return axios.get('/api/flashcards').then(response => {
       this.setState({flashcards: response.data._embedded.flashcards});
       this.setState({title: response.data._embedded.flashcards[0].title});
+      for (let i=0; i < this.state.flashcards.length; i++) {
+        this.flashcardStatus.push(-1);
+      }
     });
   }
 
-  startAnnyang() {
+  getNextQuestion() {
+    let term = this.getUnansweredTerm();
+    if (term !== '') {
+      this.askQuestion(term);
+    }
+    else {
+      this.getResult();
+    }
+  }
+
+  getResult() {
+    let result = 'You get ' + this.correctAnswers + ' correct answers over ' + this.state.flashcards.length + ' questions';
+    return this.speak(result);
+  }
+
+  getUnansweredTerm() {
+    for (let i=0; i< this.state.flashcards.length; i++) {
+      if (this.flashcardStatus[i] === -1) {
+        this.flashcardStatus[i] = 0;
+        return this.state.flashcards[i].term;
+      }
+    }
+    //if no more unanswered term
+    return '';
+  }
+
+  getCurrentTerm() {
+    for (let i=0; i< this.state.flashcards.length; i++) {
+      if (this.flashcardStatus[i] === 0) {
+        return this.state.flashcards[i].definition;
+      }
+    } 
+  }
+  
+
+  checkFlashcard(definition) {
+    for (let i=0; i < this.state.flashcards.length; i++) {
+      if (this.state.flashcards[i].definition === definition) {
+        this.flashcardStatus[i] = 1;
+        break;
+      }
+    }
+  }
+
+  getCorrectAnswer(definition) {
+    this.checkFlashcard(definition);
+    this.correctAnswers += 1;
+    this.speak('Correct')
+      .then(() => this.getNextQuestion());
+  }
+
+  getIncorrectAnswer() {
+    this.checkFlashcard(this.getCurrentTerm());
+    this.speak('Incorrect')
+      .then(() => this.getNextQuestion());
+  }
+
+  initializeCommands() {
     let commands = {
-      'introduction': () => this.setState({choice: 'intro'}),
-      'study': () => this.sayOutLoud(this.study())
+      'study': () => this.speak(this.study()),
+      'test': () => this.testUser(),
+      'next': () => this.getNextQuestion()
     };
+    for (let i = 0; i < this.state.flashcards.length; i++) {
+      let flashcard = this.state.flashcards[i];
+      commands[flashcard.definition] = () => this.getCorrectAnswer(flashcard.definition);
+    }
+    return commands;
+  }
+
+  startAnnyang() {
+    let commands = this.initializeCommands(); 
     annyang.addCommands(commands);
+    annyang.addCallback('resultNoMatch', () => this.getIncorrectAnswer());
     annyang.start();
+  }
+
+  testUser() {
+    this.speak('You are in testing mode. Starting now')
+      .then(() => this.askQuestion(this.getUnansweredTerm()));
+    
+  }
+
+  askQuestion(term) {
+    return this.speak('What is the definition of' + term);
+  }
+
+  speak(message) {
+    return new Promise((resolve, reject) => {
+      let audioSrc = 'http://commuterstudy.com/audio?msg=' + message;
+      let audio = new Audio(audioSrc);
+      audio.onended = () => resolve();
+      audio.play();
+    });
   }
 
   getInitialState() {
     return {flashcards: [],title:''};
   }
 
-  sayOutLoud(sentences) {
-    let audioSrc = 'http://commuterstudy.com/audio?msg=' + sentences;
-    let audio = new Audio(audioSrc);
-    audio.play();
-  }
-
   componentDidMount() {
-    this.getFlashcards();
-    this.sayOutLoud(this.sayIntroduction());
-    this.startAnnyang();
+    this.getFlashcards()
+      .then(() => this.speak(this.sayIntroduction()))
+      .then(() => this.startAnnyang());
   }
 
   sayIntroduction() {
